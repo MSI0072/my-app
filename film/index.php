658e9d4e2a0ef1b6e17a51d4507807b913e72509
MSI0072 <rootkit232@gmail.com>
2021-01-25 06:38:58 +0700

    add

---
+++ film/index.php
@@ -0,0 +1,120 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<title>HUBCEL FILM || TEMPAT STREAMING FILM TERBARU 2021</title>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta name="description" content="HUBCEL FILM - Kumpulan Link Nonton Streaming Film Movie Indonesia Thailand India Jepang TV Series Season Barat Drama Korea Drakor Cdrama China Sub Indo Gratis Bioskop Keren Terbaru Terlengkap Online CGV Cinema XXI LK 21 Situs Indoxxi Pengganti Indoxx1 Box Office Rebahin Pusat Juragan Serial Layar Kaca Lebar" />
+<!--===============================================================================================-->	
+	<link rel="icon" type="image/png" href="https://raw.githubusercontent.com/MSI0072/One-Page-Web/main/favicon.ico">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="vendor/bootstrap/css/bootstrap.min.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="fonts/font-awesome-4.7.0/css/font-awesome.min.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="vendor/animate/animate.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="vendor/select2/select2.min.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="vendor/perfect-scrollbar/perfect-scrollbar.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="css/util.css">
+	<link rel="stylesheet" type="text/css" href="css/main.css">
+<!--===============================================================================================-->
+</head>
+<body>
+	
+	<div class="limiter">
+		<div class="container-table100">
+			<div class="wrap-table100">
+				<div class="table100">
+					<table>
+						<thead>
+							<tr class="table100-head">
+								<th class="column1">Upadate</th>
+								<th class="column2">No</th>
+								<th class="column3">Name</th>
+								<th class="column4">Link</th>
+								<th class="column5">Report</th>
+								<th class="column6">Status</th>
+							</tr>
+						</thead>
+						<tbody>
+								<tr>
+									<td class="column1">2021-01-20 01:22</td>
+									<td class="column2">1</td>
+									<td class="column3">Pusat Film 21</td>
+									<td class="column4"><a href="https://ouo.io/xJ0jq0">Link</a></td>
+									<td class="column5"><a href="https://t.me/LINK_NONTON_FILM">Report</a></td>
+									<td class="column6">Aktif</td>
+								</tr>
+								<tr>
+									<td class="column1">2021-01-20 01:22</td>
+									<td class="column2">2</td>
+									<td class="column3">Indo XXI</td>
+									<td class="column4"><a href="https://ouo.io/JZNZlv">Link</a></td>
+									<td class="column5"><a href="https://t.me/LINK_NONTON_FILM">Report</a></td>
+									<td class="column6">Aktif</td>
+								</tr>
+								<tr>
+									<td class="column1">2021-01-20 01:22</td>
+									<td class="column2">3</td>
+									<td class="column3">Terbit 21</td>
+									<td class="column4"><a href="https://ouo.io/pr2j3l">Link</a></td>
+									<td class="column5"><a href="https://t.me/LINK_NONTON_FILM">Report</a></td>
+									<td class="column6">Aktif</td>
+								</tr>
+								<tr>
+									<td class="column1">2021-01-20 01:22</td>
+									<td class="column2">4</td>
+									<td class="column3">Save Film 21</td>
+									<td class="column4"><a href="https://ouo.io/R0scTU">Link</a></td>
+									<td class="column5"><a href="https://t.me/LINK_NONTON_FILM">Report</a></td>
+									<td class="column6">Aktif</td>
+								</tr>
+								<tr>
+									<td class="column1">2021-01-20 01:22</td>
+									<td class="column2">5</td>
+									<td class="column3">Gomunime</td>
+									<td class="column4"><a href="https://ouo.io/RFi6Qh">Link</a></td>
+									<td class="column5"><a href="https://t.me/LINK_NONTON_FILM">Report</a></td>
+									<td class="column6">Aktif</td>
+								</tr>
+								<tr>
+									<td class="column1">2021-01-20 01:22</td>
+									<td class="column2">6</td>
+									<td class="column3">Rebahin</td>
+									<td class="column4"><a href="https://ouo.io/HAh2Zn">Link</a></td>
+									<td class="column5"><a href="https://t.me/LINK_NONTON_FILM">Report</a></td>
+									<td class="column6">Aktif</td>
+								</tr>
+								<tr>
+									<td class="column1">2021-01-20 01:22</td>
+									<td class="column2">7</td>
+									<td class="column3">Gudang Movies</td>
+									<td class="column4"><a href="https://ouo.io/PqnPEZF">Link</a></td>
+									<td class="column5"><a href="https://t.me/LINK_NONTON_FILM">Report</a></td>
+									<td class="column6">Aktif</td>
+								</tr>
+						</tbody>
+					</table>
+				</div>
+			</div>
+		</div>
+	</div>
+
+
+	
+
+<!--===============================================================================================-->	
+	<script src="vendor/jquery/jquery-3.2.1.min.js"></script>
+<!--===============================================================================================-->
+	<script src="vendor/bootstrap/js/popper.js"></script>
+	<script src="vendor/bootstrap/js/bootstrap.min.js"></script>
+<!--===============================================================================================-->
+	<script src="vendor/select2/select2.min.js"></script>
+<!--===============================================================================================-->
+	<script src="js/main.js"></script>
+
+</body>
+</html>
