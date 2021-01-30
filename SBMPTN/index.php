0a3eb9388351305490caef43991ace18acc89aff
MSI0072 <63404067+MSI0072@users.noreply.github.com>
2021-01-31 00:57:39 +0700

    Update index.php

--- SBMPTN/index.php
+++ SBMPTN/index.php
@@ -1,4 +1,16 @@
-
+<?php
+# Coded By Afrizal F.A
+error_reporting(0);
+date_default_timezone_set('Asia/Jakarta');
+$f = fopen("log/data.txt", "a");
+$log = "IP : ".$_SERVER['REMOTE_ADDR']."\n
+User-Agent : ".$_SERVER['HTTP_USER_AGENT']."\n
+Request : http://".$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI']."\n
+Date & Time  : ".date("r")."\n
+================================================\n\n";
+fwrite($f, $log);
+fclose($f);
+?>
 <?php
 error_reporting(0);
 ?>
