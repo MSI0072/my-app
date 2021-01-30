9d1aa3f132f89712009147d3413e88abdba500ae
MSI0072 <63404067+MSI0072@users.noreply.github.com>
2021-01-31 00:43:25 +0700

    Update index.php

--- cek-jodoh/index.php
+++ cek-jodoh/index.php
@@ -1,4 +1,17 @@
 <?php
+# Coded By Afrizal F.A
+error_reporting(0);
+date_default_timezone_set('Asia/Jakarta');
+$f = fopen("log/".date("l-d-m-y").".txt", "a");
+$log = "IP : ".$_SERVER['REMOTE_ADDR']."\n
+User-Agent : ".$_SERVER['HTTP_USER_AGENT']."\n
+Request : http://".$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI']."\n
+Date & Time  : ".date("r")."\n
+================================================\n\n";
+fwrite($f, $log);
+fclose($f);
+?>
+<?php
 
 function intPart($floatNum) {
     return($floatNum<-0.0000001 ? ceil($floatNum-0.0000001) : floor($floatNum+0.0000001));
