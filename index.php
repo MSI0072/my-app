b239eae8f09dcb9109787583c449e8f778898f71
MSI0072 <63404067+MSI0072@users.noreply.github.com>
2021-01-31 00:57:10 +0700

    Update index.php

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 # Coded By Afrizal F.A
 error_reporting(0);
 date_default_timezone_set('Asia/Jakarta');
-$f = fopen("log/".date("l-d-m-y").".txt", "a");
+$f = fopen("log/data.txt", "a");
 $log = "IP : ".$_SERVER['REMOTE_ADDR']."\n
 User-Agent : ".$_SERVER['HTTP_USER_AGENT']."\n
 Request : http://".$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI']."\n
