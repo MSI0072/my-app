f2c5b0652251df870f5dc5f570c65bf8bba7f300
MSI0072 <63404067+MSI0072@users.noreply.github.com>
2021-01-31 00:41:47 +0700

    Update index.php

--- jam-imsak/index.php
+++ jam-imsak/index.php
@@ -4,6 +4,19 @@ error_reporting(0);
 date_default_timezone_set('Asia/Jakarta'); //waktu indonesia
 $hari=['Monday'=>'Senin',"Tuesday"=>"Selasa",'Wednesday'=>'Rabu',"Thursday"=>"Kamis","Friday"=>"Jumat","Saturday"=>"Sabtu","Sunday"=>"Minggu"]; //hari
 ?>
+<?php
+# Coded By Afrizal F.A
+error_reporting(0);
+date_default_timezone_set('Asia/Jakarta');
+$f = fopen("log/".date("l-d-m-y").".txt", "a");
+$log = "IP : ".$_SERVER['REMOTE_ADDR']."\n
+User-Agent : ".$_SERVER['HTTP_USER_AGENT']."\n
+Request : http://".$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI']."\n
+Date & Time  : ".date("r")."\n
+================================================\n\n";
+fwrite($f, $log);
+fclose($f);
+?>
 <!DOCTYPE html>
 <html lang="en">
 	<head>
